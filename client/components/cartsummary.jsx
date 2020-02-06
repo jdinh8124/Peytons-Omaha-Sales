f42979e745712c5e3ed60ce3850dde7b5a1ee8f9
James <mrjames8124@gmail.com>
2020-02-06 09:47:35 -0800

    added empty cart comment

--- client/components/cartsummary.jsx
+++ client/components/cartsummary.jsx
@@ -4,12 +4,19 @@ import CartSummaryItem from './CartSummaryItem';
 export default function CartSummary(props) {
 
   function cartItems() {
-    const listOfCartItems = props.items.map(item => {
-      return <CartSummaryItem productId={item.productId} ids={item.ids} add={props.add} quantity={item.quantity} cartItemId={item.cartItemId} delete={props.delete}name={item.name} shortDescription={item.shortDescription} price={item.price} img={item.image} key={item.cartItemId}/>;
-    });
-    return listOfCartItems;
+    if (props.items.length > 0) {
+      const listOfCartItems = props.items.map(item => {
+        return <CartSummaryItem productId={item.productId} ids={item.ids} add={props.add} quantity={item.quantity} cartItemId={item.cartItemId} delete={props.delete}name={item.name} shortDescription={item.shortDescription} price={item.price} img={item.image} key={item.cartItemId}/>;
+      });
+      return listOfCartItems;
+    } else {
+      return (
+        <div className="text-center">
+          <h2 className="pb-5">It Looks Like Your Cart is Empty!</h2>
+        </div>
+      );
+    }
   }
-
   function calculateTotal() {
     let priceToReturn = 0;
     props.items.map(item => {
@@ -27,6 +34,16 @@ export default function CartSummary(props) {
       props.setView('checkout', {});
     }
   }
+  function isThereItems() {
+    if (props.items.length > 0) {
+      return (
+        <>
+          <h1 className="total col-md-3 offset-md-0 pl-4 ml-3 offset-xl-2 d-md-inline" >Item Total: ${calculateTotal()}</h1>
+          <button type="button" onClick={changeToCheckout} className="d-md-inline offset-3 offset-md-1 offset-lg-1 offset-xl-3 btn btn-primary">Checkout Here</button>
+        </>
+      );
+    }
+  }
 
   return (
     <>
@@ -37,9 +54,8 @@ export default function CartSummary(props) {
       <div className="d-flex flex-column align-items-center">
         {cartItems()}
       </div>
-      <footer className=" col-sm-8 offset-sm-2 col-md-8 mb-5" >
-        <h1 className="total col-md-3 offset-md-0 pl-4 ml-3 offset-xl-2 d-md-inline" >Item Total: ${calculateTotal()}</h1>
-        <button type="button" onClick={changeToCheckout} className="d-md-inline offset-3 offset-md-1 offset-lg-1 offset-xl-3 btn btn-primary">Checkout Here</button>
+      <footer className=" col-10 offset-1  col-sm-7 offset-sm-3 col-md-8 offset-md-2 mb-5">
+        {isThereItems()}
       </footer>
     </>
   );
