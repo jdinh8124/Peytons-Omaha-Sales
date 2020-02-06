c143dd75e91d2a3127f8f6587aad7d76336702a2
James <mrjames8124@gmail.com>
2020-02-05 16:30:36 -0800

    finished confirmation recipet responsiveness

--- client/components/confirmationitem.jsx
+++ client/components/confirmationitem.jsx
@@ -10,12 +10,12 @@ export default function ConfirmationItem(props) {
   }
 
   return (
-    <div className="card col-12 mb-1 col-lg-7 mb-lg-5" >
+    <div className="card col-12 mb-3 col-lg-7 mb-lg-5" >
       <div className="row">
-        <div className="col-lg-5">
+        <div className="col-lg-5 d-flex justify-content-center">
           {pictureSize()}
         </div>
-        <div className="col-lg-5">
+        <div className="col-lg-5 col-lg-5 offset-2 offset-lg-0 ">
           <h2 className="card-title ">{props.name}</h2>
           <h3>Quantity: {props.quantity}</h3>
           <h3 className="card-subtitle mb-lg-2 text-muted ">${((props.price * props.quantity) / 100).toFixed(2)}</h3>
