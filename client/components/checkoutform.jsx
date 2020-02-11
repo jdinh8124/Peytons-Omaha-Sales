1379bb9e861a37bc8eae23cfc82f43a48801dbd0
James <mrjames8124@gmail.com>
2020-02-10 17:12:10 -0800

    fixed xl sized spacing for inputs

--- client/components/checkoutform.jsx
+++ client/components/checkoutform.jsx
@@ -363,155 +363,173 @@ export default class CheckoutForm extends React.Component {
           <h2>Order Total: ${this.priceTotal()}</h2>
         </div>
         <form onSubmit={this.onClickPlaceOrder} className="col-10 offset-1">
-          <div className="form-group">
-            <label>Name</label>
-            <input onChange={this.handleNameChange} type="name" className="form-control" aria-describedby="emailHelp" placeholder="Enter Name" maxLength="65"/>
+          <div className="row">
+            <div className="form-group col-xl-4">
+              <label>Name</label>
+              <input onChange={this.handleNameChange} type="name" className="form-control" aria-describedby="emailHelp" placeholder="Enter Name" maxLength="65"/>
+              {this.isNameValid()}
+            </div>
+            <div className="form-group col-xl-4">
+              <label >Email</label>
+              <input onChange={this.handleEmailChange} className="form-control" placeholder="Email" maxLength="254" />
+              {this.isThereAnEmailError()}
+            </div>
+            <div className="form-group col-xl-3">
+              <label>Phone Number</label>
+              <input id="phone" onChange={this.handlePhoneChange} className="form-control" placeholder="7149090000" maxLength="10" />
+              {this.isThereAPhoneError()}
+            </div>
           </div>
-          {this.isNameValid()}
-          <div className="form-group">
-            <label >Email</label>
-            <input onChange={this.handleEmailChange} className="form-control" placeholder="Email" maxLength="254" />
-          </div>
-          {this.isThereAnEmailError()}
-          <div className="form-group">
-            <label>Phone Number</label>
-            <input id="phone" onChange={this.handlePhoneChange} className="form-control" placeholder="7149090000" maxLength="10"/>
-          </div>
-          {this.isThereAPhoneError()}
+
           <div className="form-group">
             <label>Address</label>
             <input onChange={this.handlePrimaryShippingChange} className="form-control" placeholder="9200 Irvine St." maxLength="42" />
+            {this.isAddressValid()}
           </div>
-          {this.isAddressValid()}
           <div className="form-group">
             <label>Address 2</label>
             <input onChange={this.handleSecondaryShippingChange} className="form-control" placeholder="Apartment Suite, Studio, or Floor" maxLength="42" />
           </div>
-          <div className="form-group">
-            <label>Five Digit Zip Code</label>
-            <input onChange={this.handleZipChange} id="zip" className="form-control" placeholder="92000" maxLength="5" />
-          </div>
-          {this.isZipValid()}
-          <div className="form-group">
-            <label>City</label>
-            <input onChange={this.handleCityChange} className="form-control" placeholder="City" maxLength="50"/>
-          </div>
-          {this.isCityValid()}
-          <div className="form-group">
-            <label htmlFor="state">State</label>
-            <select onChange={this.handleStateChange} className="form-control" name="state">
-              <option>Choose State...</option>
-              <option value="AL">Alabama</option>
-              <option value="AK">Alaska</option>
-              <option value="AZ">Arizona</option>
-              <option value="AR">Arkansas</option>
-              <option value="CA">California</option>
-              <option value="CO">Colorado</option>
-              <option value="CT">Connecticut</option>
-              <option value="DE">Delaware</option>
-              <option value="DC">District Of Columbia</option>
-              <option value="FL">Flordia</option>
-              <option value="GA">Georgia</option>
-              <option value="HI">Hawaii</option>
-              <option value="ID">Idaho</option>
-              <option value="IL">Illinois</option>
-              <option value="IN">Indiana</option>
-              <option value="IA">Iowa</option>
-              <option value="IA">Kansas</option>
-              <option value="KS">Kentucky</option>
-              <option value="LA">Louisiana</option>
-              <option value="ME">Maine</option>
-              <option value="MD">Maryland</option>
-              <option value="MA">Massachusetts</option>
-              <option value="MI">Michigan</option>
-              <option value="MN">Minnesota</option>
-              <option value="MS">Mississippi</option>
-              <option value="MO">Missouri</option>
-              <option value="MT">Montana</option>
-              <option value="NE">Nebraska</option>
-              <option value="NV">Nevada</option>
-              <option value="NH">New Hampshire</option>
-              <option value="NJ">New Jersey</option>
-              <option value="NM">New Mexico</option>
-              <option value="NY">New York</option>
-              <option value="NC">North Carolina</option>
-              <option value="ND">North Dakota</option>
-              <option value="OH">Ohio</option>
-              <option value="OK">Oklahoma</option>
-              <option value="OR">Oregon</option>
-              <option value="PA">Pennsylvania</option>
-              <option value="RI">Rhode Island</option>
-              <option value="SC">South Carolina</option>
-              <option value="SD">South Dakota</option>
-              <option value="TN">Tennessee</option>
-              <option value="TX">Texas</option>
-              <option value="UT">Utah</option>
-              <option value="VT">Vermont</option>
-              <option value="VA">Virginia</option>
-              <option value="WA">Washington</option>
-              <option value="WV">West Virginia</option>
-              <option value="WI">Wisconsin</option>
-              <option value="WY">Wyoming</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <label>Country</label>
-            <input onChange={this.handleCountryChange} className="form-control" value="USA" placeholder="USA" maxLength="15" />
+          <div className="row">
+            <div className="form-group col-xl-2">
+              <label>Five Digit Zip Code</label>
+              <input onChange={this.handleZipChange} id="zip" className="form-control" placeholder="92000" maxLength="5" />
+              {this.isZipValid()}
+            </div>
+            <div className="form-group col-xl-3">
+              <label>City</label>
+              <input onChange={this.handleCityChange} className="form-control" placeholder="City" maxLength="50"/>
+              {this.isCityValid()}
+            </div>
+            <div className="form-group col-xl-4">
+              <label htmlFor="state">State</label>
+              <select onChange={this.handleStateChange} className="form-control" name="state">
+                <option>Choose State...</option>
+                <option value="AL">Alabama</option>
+                <option value="AK">Alaska</option>
+                <option value="AZ">Arizona</option>
+                <option value="AR">Arkansas</option>
+                <option value="CA">California</option>
+                <option value="CO">Colorado</option>
+                <option value="CT">Connecticut</option>
+                <option value="DE">Delaware</option>
+                <option value="DC">District Of Columbia</option>
+                <option value="FL">Flordia</option>
+                <option value="GA">Georgia</option>
+                <option value="HI">Hawaii</option>
+                <option value="ID">Idaho</option>
+                <option value="IL">Illinois</option>
+                <option value="IN">Indiana</option>
+                <option value="IA">Iowa</option>
+                <option value="IA">Kansas</option>
+                <option value="KS">Kentucky</option>
+                <option value="LA">Louisiana</option>
+                <option value="ME">Maine</option>
+                <option value="MD">Maryland</option>
+                <option value="MA">Massachusetts</option>
+                <option value="MI">Michigan</option>
+                <option value="MN">Minnesota</option>
+                <option value="MS">Mississippi</option>
+                <option value="MO">Missouri</option>
+                <option value="MT">Montana</option>
+                <option value="NE">Nebraska</option>
+                <option value="NV">Nevada</option>
+                <option value="NH">New Hampshire</option>
+                <option value="NJ">New Jersey</option>
+                <option value="NM">New Mexico</option>
+                <option value="NY">New York</option>
+                <option value="NC">North Carolina</option>
+                <option value="ND">North Dakota</option>
+                <option value="OH">Ohio</option>
+                <option value="OK">Oklahoma</option>
+                <option value="OR">Oregon</option>
+                <option value="PA">Pennsylvania</option>
+                <option value="RI">Rhode Island</option>
+                <option value="SC">South Carolina</option>
+                <option value="SD">South Dakota</option>
+                <option value="TN">Tennessee</option>
+                <option value="TX">Texas</option>
+                <option value="UT">Utah</option>
+                <option value="VT">Vermont</option>
+                <option value="VA">Virginia</option>
+                <option value="WA">Washington</option>
+                <option value="WV">West Virginia</option>
+                <option value="WI">Wisconsin</option>
+                <option value="WY">Wyoming</option>
+              </select>
+            </div>
+            <div className="form-group col-xl-2">
+              <label>Country</label>
+              <input onChange={this.handleCountryChange} className="form-control" value="USA" placeholder="USA" maxLength="15" />
+            </div>
           </div>
           <hr className="my-4"></hr>
           <h3>Payment</h3>
-          <div className="form-group">
-            <label>Name on Card</label>
-            <input type="name" onChange={this.handleNameOnCardChange} className="form-control" aria-describedby="" placeholder="John Doe" />
+          <div className="row">
+            <div className="form-group col-xl-5">
+              <label>Name on Card</label>
+              <input type="name" onChange={this.handleNameOnCardChange} className="form-control" aria-describedby="" placeholder="John Doe" />
+            </div>
+            <div className="form-group col-xl-5">
+              <label>Card Number</label>
+              <input onChange={this.handleCreditChange} id="creditCard" className="form-control" aria-describedby="card number" placeholder="12345668495" maxLength="16"/>
+              {this.isCreditValid()}
+            </div>
           </div>
-          <div className="form-group">
-            <label>Card Number</label>
-            <input onChange={this.handleCreditChange} id="creditCard" className="form-control" aria-describedby="card number" placeholder="12345668495" maxLength="16"/>
+          <div className="row">
+            <div className="form-group col-xl-3">
+              <label>Month</label>
+              <select onChange={this.handleMonthChange} className="form-control">
+                <option>##</option>
+                <option value="01">01</option>
+                <option value="02">02</option>
+                <option value="03">03</option>
+                <option value="04">04</option>
+                <option value="05">05</option>
+                <option value="06">06</option>
+                <option value="07">07</option>
+                <option value="08">08</option>
+                <option value="09">09</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+              </select>
+            </div>
+
+            <div className="col-xl-3">
+              <label>Year</label>
+              <select onChange={this.handleYearChange} className="form-control">
+                <option>####</option>
+                <option value="2020">2020</option>
+                <option value="2021">2021</option>
+                <option value="2022">2022</option>
+                <option value="2023">2023</option>
+                <option value="2024">2024</option>
+                <option value="2025">2025</option>
+                <option value="2026">2026</option>
+              </select>
+            </div>
+            <div className="col-xl-3">
+
+              <label>CVC</label>
+              <input onChange={this.handleCvcChange} id="cvc" className="form-control" aria-describedby="card number" placeholder="###" maxLength="3" />
+              {this.iscvcValid()}
+              {this.isFormEmpty()}
+            </div>
           </div>
-          {this.isCreditValid()}
-          <div className="form-group">
-            <label>Month</label>
-            <select onChange={this.handleMonthChange}className="form-control">
-              <option>##</option>
-              <option value="01">01</option>
-              <option value="02">02</option>
-              <option value="03">03</option>
-              <option value="04">04</option>
-              <option value="05">05</option>
-              <option value="06">06</option>
-              <option value="07">07</option>
-              <option value="08">08</option>
-              <option value="09">09</option>
-              <option value="10">10</option>
-              <option value="11">11</option>
-              <option value="12">12</option>
-            </select>
-            <label>Year</label>
-            <select onChange={this.handleYearChange} className="form-control">
-              <option>####</option>
-              <option value="2020">2020</option>
-              <option value="2021">2021</option>
-              <option value="2022">2022</option>
-              <option value="2023">2023</option>
-              <option value="2024">2024</option>
-              <option value="2025">2025</option>
-              <option value="2026">2026</option>
-            </select>
-            <label>CVC</label>
-            <input onChange={this.handleCvcChange} id="cvc" className="form-control" aria-describedby="card number" placeholder="###" maxLength="3" />
-          </div>
-          {this.iscvcValid()}
-          {this.isFormEmpty()}
-          <hr className="my-4"></hr>
-          <input className="form-check-input" type="checkbox" required />
-          <label>
+          <div >
+            <hr className="my-4"></hr>
+            <input className="form-check-input" type="checkbox" required />
+            <label>
             I acknowledge that this is a demo application, and the information above is not my genuine financial or personal information.
-          </label>
-          <hr className="my-4"></hr>
-          <div className="mt-5 mb-5">
-            <h3 className="pointer d-inline small-checkout mr-3" onClick={this.backToMainShop}>{'<Continue Shopping'}</h3>
-            <button className="btn btn-primary offset-lg-5 offset-sm-1 d-inline ">Submit</button>
+            </label>
+            <hr className="my-4"></hr>
+            <div className="mt-5 mb-5">
+              <h3 className="pointer d-inline small-checkout mr-3" onClick={this.backToMainShop}>{'<Continue Shopping'}</h3>
+              <button className="btn btn-primary offset-lg-5 offset-sm-1 d-inline ">Submit</button>
+            </div>
           </div>
+
         </form>
       </>
     );
