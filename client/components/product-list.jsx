89a2091a0593d01fe0b129371a6c4905795ea6a6
James <mrjames8124@gmail.com>
2020-03-21 11:17:54 -0700

    changed to hook but memory leak fix

--- client/components/product-list.jsx
+++ client/components/product-list.jsx
@@ -1,16 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ProductListItems from './product-list-item';
 export default function ProductLists(props) {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     products: []
-  //   };
-  // }
-
-  // functioncomponentDidMount() {
-  //   getProducts();
-  // }
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -23,12 +13,7 @@ export default function ProductLists(props) {
         return response.json();
       })
       .then(myJson => {
-        // this.setState({
-        //   products: myJson
-
-        // });
         setProducts(myJson);
-        console.log(myJson);
       })
       .catch(reason => {
         console.error(reason.message);
@@ -38,7 +23,7 @@ export default function ProductLists(props) {
 
   function renderCards() {
     const cards = products.map(product => {
-      return <ProductListItems name={product.name} cost={(product.price / 100).toFixed(2)} description={product.shortDescription} onClick={this.props.setView} img={product.image} id={product.productId} key={product.productId}/>;
+      return <ProductListItems name={product.name} cost={(product.price / 100).toFixed(2)} description={product.shortDescription} onClick={props.setView} img={product.image} id={product.productId} key={product.productId}/>;
     });
     return cards;
   }
